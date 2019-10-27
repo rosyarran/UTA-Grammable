33fca5c73b4ebf53e4237336b22274c810dcd0ff
Rosy Arreola <rosy.arreola@yahoo.com>
2019-10-27 03:23:53 +0000

    migration nation a bit broken but hopefully fixed

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -51,7 +51,7 @@ class GramsController < ApplicationController
   private
   
   def gram_params
-    params.require(:gram).permit(:message)
+    params.require(:gram).permit(:message, :picture)
   end
   
   def render_not_found
