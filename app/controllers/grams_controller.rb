00fc57d11cded145985999acd06836315abc0c2f
Rosy Arreola <rosy.arreola@yahoo.com>
2019-10-27 03:48:55 +0000

    images show please dont break please dont break

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -2,6 +2,7 @@ class GramsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   
   def index
+    @grams = Gram.all
   end
   
   def new
